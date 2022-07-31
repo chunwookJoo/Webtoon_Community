7f14619f22e13772692d553fb0e4a939918832e5
jeongha-joo <jha970107@gmail.com>
2022-07-31 23:20:40 +0900

    Nav, 플랫폼 Select 완성

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { Collapse, Button } from "reactstrap";
-import { PlatformLinkOptions } from "./PlatformLinkOptions";
 import { Link, useLocation } from "react-router-dom";
-import { ReactComponent as Logo } from "../assets/img/logo.svg";
+import { ReactComponent as Logo } from "../assets/img/logo2.svg";
 import "../assets/scss/components/nav.scss";
 
 const LogoComponent = () => {
@@ -17,48 +13,9 @@ const LogoComponent = () => {
 	);
 };
 
-const PlatformLink = (props) => {
-	const option = props.option;
-	const { icon, name, src } = option;
-	return (
-		<li>
-			<Link to={src}>
-				<p>
-					{icon}
-					{name}
-				</p>
-			</Link>
-		</li>
-	);
-};
-
-const PlatformSelect = (props) => {
-	const { SelectedIcon, PlatformLinkOptions } = props;
-	const [isPlatformOpen, setIsPlatformOpen] = useState(false);
-
+const SignIn = () => {
 	return (
 		<div>
-			<span onClick={() => setIsPlatformOpen(!isPlatformOpen)}>
-				{SelectedIcon} 플랫폼
-			</span>
-			<Collapse isOpen={isPlatformOpen} className="platform-collapse">
-				<ul className="platform-list">
-					<PlatformLink option={PlatformLinkOptions.all} />
-					<PlatformLink option={PlatformLinkOptions.naver} />
-					<PlatformLink option={PlatformLinkOptions.kakao} />
-					<PlatformLink option={PlatformLinkOptions.kakaoPage} />
-				</ul>
-			</Collapse>
-		</div>
-	);
-};
-
-const SearchLogin = () => {
-	return (
-		<div>
-			<div>
-				<FontAwesomeIcon icon={faMagnifyingGlass} />
-			</div>
 			<div>
 				<Link to="/login">
 					<span>로그인</span>
@@ -72,54 +29,12 @@ const SearchLogin = () => {
 };
 
 const Nav = () => {
-	const { pathname, search } = useLocation();
-	const platform = Object.hasOwn(PlatformLinkOptions, pathname.split("/")[1]);
-	const SelectedIcon = !platform
-		? PlatformLinkOptions.all.icon
-		: PlatformLinkOptions[pathname.split("/")[1]].icon;
-
-	const week = ["월", "화", "수", "목", "금", "토", "일"];
-	const todayNum = new Date().getDay();
-	const weekDayLinkOptions = week.map((day, weekNum) => ({
-		name: day,
-		src: `?week=${weekNum}`,
-	}));
-	weekDayLinkOptions.unshift({
-		name: "신작",
-		src: "?week=new",
-	});
-	weekDayLinkOptions.push({
-		name: "완결",
-		src: "?week=fin",
-	});
-
-	const today = week[todayNum - 1];
-
-	const WeekList = weekDayLinkOptions.map((weekItem, index) => {
-		let active = "";
-		!search
-			? weekItem.name === today && (active = "active")
-			: search === weekItem.src && (active = "active");
-		return (
-			<li key={index}>
-				<Link to={weekItem.src} className={active}>
-					{weekItem.name}
-				</Link>
-			</li>
-		);
-	});
-
 	return (
 		<section className="nav-section">
 			<div className="nav-container">
 				<LogoComponent />
-				<PlatformSelect
-					SelectedIcon={SelectedIcon}
-					PlatformLinkOptions={PlatformLinkOptions}
-				/>
-				<SearchLogin />
+				<SignIn />
 			</div>
-			<ul>{WeekList}</ul>
 		</section>
 	);
 };
--- src/pages/WebtoonPage.js
+++ src/pages/WebtoonPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
+import { Collapse, Button } from "reactstrap";
+import { PlatformLinkOptions } from "../components/PlatformLinkOptions";
 
 import Loading from "../components/Loading";
 import Webtoon from "../components/Webtoon";
@@ -8,19 +10,152 @@ import axios from "axios";
 import qs from "qs";
 
 import "../assets/scss/pages/webtoonPage.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const WEBTOON_API_URL = "https://korea-webtoon-api.herokuapp.com";
 const todayNum = new Date().getDay();
 const week = ["0", "1", "2", "3", "4", "5", "6"];
 const EMPTY = <></>;
-const todayWeek = week[todayNum - 1];
+const todayWeek = week[todayNum === 0 ? 6 : todayNum - 1];
 let part = 2;
+
+const PlatformLink = (props) => {
+	const option = props.option;
+	const { icon, name, src } = option;
+	return (
+		<li onClick={() => props.getData(name)}>
+			<Link to={src}>
+				<span>{icon}</span>
+				<p>{name}</p>
+			</Link>
+		</li>
+	);
+};
+
+const WeekLink = () => {
+	let { search, pathname } = useLocation();
+	const week = ["월", "화", "수", "목", "금", "토", "일"];
+	const todayNum = new Date().getDay();
+
+	const weekDayLinkOptions = week.map((day, weekNum) => ({
+		name: day,
+		src: `?week=${weekNum}`,
+	}));
+
+	weekDayLinkOptions.unshift({
+		name: "신작",
+		src: "?week=new",
+	});
+
+	weekDayLinkOptions.push({
+		name: "완결",
+		src: "?week=fin",
+	});
+
+	const today = week[todayNum === 0 ? 6 : todayNum - 1];
+
+	// 요일 nav
+	const WeekList = weekDayLinkOptions.map((weekItem, index) => {
+		let active = "";
+		!search
+			? weekItem.name === today && (active = "active")
+			: search === weekItem.src && (active = "active");
+
+		return (
+			<li key={index}>
+				<Link to={weekItem.src} className={active}>
+					{weekItem.name}
+				</Link>
+			</li>
+		);
+	});
+
+	return <ul className="week-list-wrap">{WeekList}</ul>;
+};
+
+const WeekPlatformSelect = (props) => {
+	let { search, pathname } = useLocation();
+	let selected = "";
+	switch (pathname.split("/")[1]) {
+		case "naver":
+			selected = "네이버";
+			break;
+		case "kakao":
+			selected = "카카오";
+			break;
+		case "kakaoPage":
+			selected = "카카오페이지";
+			break;
+		default:
+			selected = "전체";
+			break;
+	}
+	const { SelectedIcon, PlatformLinkOptions } = props;
+	const [isPlatformOpen, setIsPlatformOpen] = useState(false);
+	const [platformNameSelected, setPlatformNameSelected] = useState(selected);
+
+	const getPlatformName = (name) => {
+		setPlatformNameSelected(name);
+		setIsPlatformOpen(false);
+	};
+
+	return (
+		<>
+			<div className="platform-select-container">
+				<div
+					className="platform-select"
+					onClick={() => setIsPlatformOpen(!isPlatformOpen)}
+				>
+					<span>{SelectedIcon}</span>
+					<span>{platformNameSelected}</span>
+					<span>
+						<FontAwesomeIcon icon={faCaretDown} />
+					</span>
+					<Collapse isOpen={isPlatformOpen} className="platform-collapse">
+						<ul className="platform-list">
+							<PlatformLink
+								option={PlatformLinkOptions.all}
+								getData={getPlatformName}
+							/>
+							<PlatformLink
+								option={PlatformLinkOptions.naver}
+								getData={getPlatformName}
+							/>
+							<PlatformLink
+								option={PlatformLinkOptions.kakao}
+								getData={getPlatformName}
+							/>
+							<PlatformLink
+								option={PlatformLinkOptions.kakaoPage}
+								getData={getPlatformName}
+							/>
+						</ul>
+					</Collapse>
+				</div>
+				<div className="search">
+					<span>
+						<FontAwesomeIcon icon={faMagnifyingGlass} />
+					</span>
+				</div>
+			</div>
+			<div>
+				<WeekLink />
+			</div>
+		</>
+	);
+};
 
 const WebtoonPage = () => {
 	let { search, pathname } = useLocation();
 	const visibleWebtoonCount = part * 12;
 	const query = qs.parse(search, { ignoreQueryPrefix: true });
 	const [webtoonList, setWebtoonList] = useState([]);
+	const platform = Object.hasOwn(PlatformLinkOptions, pathname.split("/")[1]);
+	const SelectedIcon = !platform
+		? PlatformLinkOptions.all.icon
+		: PlatformLinkOptions[pathname.split("/")[1]].icon;
 
 	useEffect(() => {
 		part = 2;
@@ -28,8 +163,8 @@ const WebtoonPage = () => {
 			const PLATFORM_URL =
 				pathname === "/"
 					? "/all"
-					: pathname === "/kakaopage"
-					? "kakao-page"
+					: pathname === "/kakaoPage"
+					? "/kakao-page"
 					: pathname;
 			!query.week && (query.week = todayWeek);
 			const WEEK_URL =
@@ -37,6 +172,7 @@ const WebtoonPage = () => {
 			const { data } = await axios.get(
 				WEBTOON_API_URL + PLATFORM_URL + WEEK_URL,
 			);
+			console.log(data);
 			const WebtoonList = data.map((webtoon) => (
 				<Webtoon webtoonData={webtoon} />
 			));
@@ -44,6 +180,7 @@ const WebtoonPage = () => {
 		};
 		DataParsing();
 	}, [query.week, pathname]);
+
 	const [moreRef, isMoreRefShow] = useInView();
 	isMoreRefShow && part++;
 
@@ -55,7 +192,6 @@ const WebtoonPage = () => {
 		) : (
 			EMPTY
 		);
-
 	const VisibleWebtoonList = webtoonList.slice(0, part * 12);
 	VisibleWebtoonList.push(More);
 
@@ -63,6 +199,10 @@ const WebtoonPage = () => {
 		<main>
 			<section className="contents-container"></section>
 			<section className="contents-container">
+				<WeekPlatformSelect
+					SelectedIcon={SelectedIcon}
+					PlatformLinkOptions={PlatformLinkOptions}
+				/>
 				{webtoonList.length === 0 ? (
 					<ul className="webtoon-list">
 						<Loading />
