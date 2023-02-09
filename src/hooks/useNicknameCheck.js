fb96388a0afd8a6233cdd759024d783edda9e5f0
JooChunWook <jha970107@gmail.com>
2023-02-10 01:56:29 +0900

    닉네임 체크 hook 생성

---
+++ src/hooks/useNicknameCheck.js
@@ -0,0 +1,15 @@
+import { postCheckNickName } from "../api/profile";
+
+const useNicknameCheck = async (nickname) => {
+  const postCheckNickNameAPIBody = {
+    nickname,
+  };
+
+  if (nickname === "") return "empty";
+
+  const response = await postCheckNickName(postCheckNickNameAPIBody);
+  if (response.RESULT === 200) return "available";
+  else if (response.RESULT === 403) return "unavailable";
+};
+
+export default useNicknameCheck;
