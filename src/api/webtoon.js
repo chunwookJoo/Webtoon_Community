444c6c571ba59faee07dc7289f85b3eba2e5ddd6
JooChunWook <jha970107@gmail.com>
2023-02-10 01:54:32 +0900

    웹툰 api 로직 분리

--- src/api/webtoon.js
+++ src/api/webtoon.js
@@ -1,5 +1,15 @@
+import { ERROR_MESSAGE, USER_ID } from "../utils/constants";
+import { getLocalStorage } from "../utils/storage";
+import showToast from "../utils/toast";
 import api from "./api";
 
+/**
+ * 웹툰 리스트 출력
+ * @param {string} pathname
+ * @param {any} query
+ * @param {number} page
+ * @returns
+ */
 const getWebtoonList = async (pathname, query, page) => {
   const todayNum = new Date().getDay();
   const week = ["0", "1", "2", "3", "4", "5", "6"];
@@ -34,10 +44,50 @@ const getWebtoonList = async (pathname, query, page) => {
     const { data } = await api.get(`/api${PLATFORM_URL}${WEEK_URL}&page=${page}`);
     return data;
   } catch (error) {
-    throw new Error("웹툰 리스트 출력 에러");
+    showToast(ERROR_MESSAGE, "red");
+    throw new Error(error, "웹툰 리스트 출력 에러");
   }
 };
 
-const getWebtoonDetail = () => {};
+const getWebtoonDetail = async (webtoonId) => {
+  try {
+    const { data } = await api.get(`/search/webtoon/${webtoonId}`);
+    return data;
+  } catch (error) {
+    showToast(ERROR_MESSAGE, "red");
+    throw new Error(error, "마이웹툰 저장 에러");
+  }
+};
 
-export { getWebtoonList };
+/**
+ * 마이웹툰에 웹툰 저장
+ * @param {any} postMyWebtoonAPIBody
+ * @returns
+ */
+const postMyWebtoon = async (postMyWebtoonAPIBody) => {
+  try {
+    const { data } = await api.post(
+      `/auth/insert/mywebtoon/${getLocalStorage(USER_ID)}`,
+      postMyWebtoonAPIBody,
+    );
+    return data;
+  } catch (error) {
+    showToast(ERROR_MESSAGE, "red");
+    throw new Error(error, "마이웹툰 저장 에러");
+  }
+};
+
+const removeMyWebtoon = async (removeMyWebtoonAPIBody) => {
+  try {
+    const { data } = await api.post(
+      `/auth/delete/mywebtoon/${getLocalStorage(USER_ID)}`,
+      removeMyWebtoonAPIBody,
+    );
+    return data;
+  } catch (error) {
+    showToast(ERROR_MESSAGE, "red");
+    throw new Error(error, "마이웹툰 저장 에러");
+  }
+};
+
+export { getWebtoonList, getWebtoonDetail, postMyWebtoon, removeMyWebtoon };
