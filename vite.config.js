c79d70fc3f61b73c9a3cb40a95f3b61783bd67f7
JooChunWook <jha970107@gmail.com>
2023-02-14 14:42:30 +0900

    vite로 변경, build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import viteSvgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: ".",
+  build: {
+    emptyOutDir: true,
+    outDir: "../webtoon_server/public",
+  },
   plugins: [react(), viteSvgr()],
   server: {
     port: 3000,
